2b1709e25e8caee2518c228d4b82d14007fffb0e
uzura <uzura_contact@yahoo.co.jp>
2015-05-17 02:10:32 +0900

    人間側で振動ボタン押すと猫側で振動するまで動作する

--- app/src/main/java/jp/co/spajam/androidapp/broadcastreceiver/OnReceiveJobBroadcastReceiver.java
+++ app/src/main/java/jp/co/spajam/androidapp/broadcastreceiver/OnReceiveJobBroadcastReceiver.java
@@ -3,6 +3,7 @@ package jp.co.spajam.androidapp.broadcastreceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Vibrator;
 import android.util.Log;
 
 import org.json.JSONException;
@@ -25,10 +26,26 @@ public class OnReceiveJobBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         ArrayList<Job> jobs = (ArrayList<Job>)intent.getSerializableExtra("JOBS");
 
-        for (Job job : jobs) {
+        // user_idは見ていません。すべてのuser_idに反応します
+        for (Job job : jobs) { // [HINT]for文をかいているけど実際は１個ずつjobが来ます
             int jobId = job.getJob_id();
-            Log.d("test", "jobId:" + jobId);
-            //TODO jobIDによってカメラ起動したり光らせたりする
+            switch (jobId){
+                case Job.LIGHT_JOB_ID:
+                    //TODO ライトを光らせる
+                    Log.d("job","ライトを光らせる");
+                    break;
+                case Job.VIB_JOB_ID:
+                    // 振動させる
+                    Log.d("job","振動させる");
+                    Vibrator vibrator = (Vibrator) petModeFragment.getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+                    long[] pattern = {300, 1000, 200, 1000, 300, 1000}; // OFF/ON/OFF/ON...
+                    vibrator.vibrate(pattern, -1);
+                    break;
+                case Job.VOICE_JOB_ID:
+                    // TODO 音を鳴らす
+                    Log.d("job","音を鳴らす");
+                    break;
+            }
         }
     }
 
