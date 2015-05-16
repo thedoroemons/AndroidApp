fce4fc1e3c330db25f7f1f776a7a708d8998befc
uzura <uzura_contact@yahoo.co.jp>
2015-05-17 02:16:20 +0900

    ゴミ削除

--- app/src/main�java/jp/co/spajam/androidapp/listener/OnReceiveJob.java
+++
@@ -1,9 +0,0 @@
-package jp.co.spajam.androidapp.listener;
-
-import java.util.ArrayList;
-
-import jp.co.spajam.androidapp.data.Job;
-
-public interface OnReceiveJob {
-    public void onReceiveJob(ArrayList<Job> jobList);
-}
